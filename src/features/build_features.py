0b09d7dc7ff2dd998afe5ead5486e8156bc559f7
Satish Terala <satish@thedummyvariable.com>
2016-07-12 00:26:20 -0400

    Set up the proper structure and added in the data cleaning part

--- src/features/build_features.py
+++ src/features/build_features.py
@@ -1,11 +1,52 @@
-import logging
+from sklearn.ensemble import RandomForestClassifier
+from sklearn.naive_bayes import BernoulliNB, MultinomialNB, GaussianNB
+from sklearn.tree import DecisionTreeClassifier
+from __future__ import division
 
-def main():
-    print("In build features")
+import matplotlib.pyplot as plt
+import numpy as np
+from sklearn.cross_validation import KFold
+from sklearn.ensemble import RandomForestClassifier
+from sklearn.feature_selection import SelectKBest
+from sklearn.feature_selection import chi2
+from sklearn.feature_selection import f_classif
+from sklearn.linear_model import LogisticRegression
+from sklearn.linear_model import RidgeClassifier
+from sklearn.linear_model import SGDClassifier
+from sklearn.metrics import accuracy_score
+from sklearn.multiclass import OneVsRestClassifier
+from sklearn.naive_bayes import BernoulliNB
+from sklearn.naive_bayes import GaussianNB
+from sklearn.naive_bayes import MultinomialNB
+from sklearn.svm import LinearSVC
 
 
-if __name__ == '__main__':
-    main()
+class FeatureSelection:
+
+
+    classifiers =[
+        BernoulliNB(),
+        MultinomialNB(),
+        GaussianNB(),
+        DecisionTreeClassifier(),
+        RandomForestClassifier(n_estimators=10),
+        OneVsRestClassifier(LinearSVC(random_state=0)),
+        OneVsRestClassifier(LogisticRegression()),
+        OneVsRestClassifier(SGDClassifier()),
+        OneVsRestClassifier(RidgeClassifier()),
+    ]
+
+
+    def univariateFeatureSelection(self,X,y,nfolds,clf,nfeats,clfname,scoreFunc):
+        kfold = KFold(X.shape[0],n_folds=nfolds)
+        acc = 0
+        i = 0
+        print("%s (#-features = %d).... %"% (clfname,nfeats))
+        for train,test in kfold:
+            i += 1
+            X_train,X_test,y_train,y_test =  X[train],X[test],y[train],y[test]
+            clf.fit(X_train,y_train)
+
 
 
 
--- src/main_runner.py
+++ src/main_runner.py
@@ -4,34 +4,34 @@ import os
 from logging.config import fileConfig
 import click
 
-
-#from dotenv import find_dotenv, load_dotenv
+# from dotenv import find_dotenv, load_dotenv
 from data.lc_data_janitor import LcDataExtractor
 
 
 @click.command()
 @click.argument('input_filepath', type=click.Path(exists=True))
 @click.argument('output_filepath', type=click.Path())
-def main(input_filepath,output_filepath):
+def main(input_filepath, output_filepath):
     fileConfig('/Users/Satish/Work/lending_club_classifier/src/logging.ini')
     logger = logging.getLogger()
     logger.debug("Testing Logging")
     logger = logging.getLogger(__name__)
     logger.info('making final data set from raw data')
     lcd = LcDataExtractor()
-    lcd.create(input_filepath)
+    df = lcd.create(input_filepath)
+
 
 
 if __name__ == '__main__':
-    #log_fmt = '%(asctime)s - %(name)s - %(levelname)s - %(message)s'
-    #logging.basicConfig(level=logging.INFO, format=log_fmt)
+    # log_fmt = '%(asctime)s - %(name)s - %(levelname)s - %(message)s'
+    # logging.basicConfig(level=logging.INFO, format=log_fmt)
 
     # not used in this stub but often useful for finding various files
-    #project_dir = os.path.join(os.path.dirname(__file__), os.pardir, os.pardir)
-    
+    # project_dir = os.path.join(os.path.dirname(__file__), os.pardir, os.pardir)
+
     # find .env automagically by walking up directories until it's found, then
     # load up the .env entries as environment variables
- #   load_dotenv(find_dotenv())
+    #   load_dotenv(find_dotenv())
 
     main()
 
